2a9ee5e65eb4e3bb400fa47aa07fe4311344cb21
Kirill45-re <63586186+Kirill45-re@users.noreply.github.com>
2020-04-19 13:14:29 +0300

    Add files via upload

---
+++ binar.py
@@ -0,0 +1,17 @@
+lists = [1,3,5,7,9,11,13,15,17,20,21]
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        midle = low + high
+        guess = list[midle]
+        if guess == list[midle]:
+            return midle
+        elif guess > item:# значение больше
+            high = mid + 1 
+        else:# значение меньше
+            low = mid - 1 
+    return None # Значение не существует
+
+
+    print(binary_search(list,2)) 
